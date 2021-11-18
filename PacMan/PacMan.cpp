c700fc4ab876c832cb4f0d98c92694a668a1d989
Alex Agustí Moncasi <alexagusti00@gmail.com>
2021-11-18 21:06:32 +0100

    Rellenado de mapa

--- PacMan/PacMan.cpp
+++ PacMan/PacMan.cpp
@@ -1,19 +1,33 @@
-// PacMan.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+using namespace std;
+
+#define CONSOLE_HEIGHT 20
+#define CONSOLE_WIDHT 20
+
+//Enumerador de tipos de carácteres que conforman el mapa
+enum MAP_TILES { EMPTY = ' ', WALL = '#', POINT = '.' };
+
+//Array multidimensional que delimitará el mapa en consola
+MAP_TILES ConsoleScreen[CONSOLE_HEIGHT][CONSOLE_WIDHT];
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Primero generamos el mapa y los puntos
+    RellenarMapa();
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+/*Para rellenar el mapa utilizaremos un dobre bucle "for" que nos recorra toda la consola. En este bucle
+comprovaremos si estamos en una posición x = 0, y = 0, x = ancho - 1 o y = alto - 1. En caso de estar en
+una de estas 4 posiciones colocaremos un # (o pared), sino lo dejaremos vacío.*/
+void RellenarMapa() {
+    for (int i = 0; i < CONSOLE_HEIGHT; i++) {
+        for (int j = 0; j < CONSOLE_WIDHT; j++) {
+            if (i == 0 || i == CONSOLE_HEIGHT - 1 || j == 0 || j == CONSOLE_WIDHT - 1) {
+                ConsoleScreen[i][j] = MAP_TILES::WALL;
+            }
+            else {
+                ConsoleScreen[i][j] = MAP_TILES::EMPTY;
+            }
+        }
+    }
+}
